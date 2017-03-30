d00b230650be5be3531850fc75015f63a6292594
Timothy Hornick <thornick18@k132-10.stjohnsprep.org>
2017-03-30 10:28:53 -0400

    Asteroids in Asteroids

---
+++ src/asteroids/Asteroid.java
@@ -0,0 +1,65 @@
+package asteroids;
+
+import static psilox.math.Random.*;
+import psilox.core.Psilox;
+import psilox.graphics.Color;
+import psilox.graphics.Draw;
+import psilox.graphics.Shader;
+import psilox.math.Vec;
+import psilox.node.Node;
+
+public class Asteroid extends Node {
+	
+	private static Shader distort;
+	
+	static {
+		if(distort == null) {
+			distort = new Shader("asteroids/asteroid.shd");
+		}
+	}
+	
+	public boolean small;
+	private Vec velocity;
+	private float spinRate;
+	
+	public void update() {
+		position.add(velocity);
+		rotation += spinRate;
+		
+		Vec viewSize = viewSize();
+		position.x = (position.x < 0 ? viewSize.x - 1 : position.x) % viewSize.x;
+		position.y = (position.y < 0 ? viewSize.y - 1 : position.y) % viewSize.y;
+	}
+	
+	public void render() {
+		distort.enable();
+		distort.setUniform1f("time", Psilox.ticks() / 100f);
+		Draw.ellipse(Color.BROWN, Vec.ZERO, small ? 20 : 40, 10);
+		distort.disable();
+	}
+	
+	public void split() {
+		if(!small) {
+			Asteroid a = new Asteroid(), b = new Asteroid();
+			a.position.set(position.sum(new Vec(10)));
+			a.velocity = velocity.rot(45);
+			a.spinRate = spinRate / 2;
+			a.small = true;
+			b.position.set(position.sum(new Vec(-10)));
+			b.velocity = velocity.rot(-45);
+			b.spinRate = spinRate / 3;
+			b.small = true;
+			getParent().addChildren(a, b);
+		}
+		freeSelf();
+	}
+	
+	public static Asteroid spawn(Vec dim) {
+		Asteroid a = new Asteroid();
+		a.position.set(dim.pro(new Vec(floatVal(1), floatVal(1))));
+		a.velocity = new Vec(floatVal(-5, 5), floatVal(-5, 5));
+		a.spinRate = floatVal(-5, 5);
+		return a;
+	}
+	
+}
--- src/asteroids/Game.java
+++ src/asteroids/Game.java
@@ -1,14 +1,26 @@
 package asteroids;
 
+import java.awt.Font;
+import java.util.List;
+
 import psilox.core.Config;
 import psilox.core.Psilox;
+import psilox.graphics.Color;
+import psilox.math.Vec;
 import psilox.node.Node;
+import psilox.node.ui.Container;
+import psilox.node.ui.Label;
+import psilox.node.utility.Timer;
+import psilox.utils.Pointer.IntPointer;
 
 public class Game extends Node {
 
 	private Player player;
 	private Sky sky;
 	private Node bulletList;
+	private Node asteroidList;
+	private Container UI;
+	private IntPointer score;
 	
 	public void enteredTree() {
 		this.tag = "game";
@@ -21,11 +33,60 @@ public class Game extends Node {
 		bulletList = new Node();
 		bulletList.tag = "bulletList";
 		bulletList.position.z = .5f;
+	
+		asteroidList = new Node();
+		asteroidList.tag = "asteroidList";
+		asteroidList.position.z = .25f;
 		
 		sky = new Sky();
 		sky.tag = "sky";
+	
+		Timer spawner = new Timer(5f, false, () -> asteroidList.addChild(Asteroid.spawn(viewSize())));
+		spawner.tag = "asteroidSpawner";
+		spawner.start();
 		
-		addChildren(sky, bulletList, player);
+		UI = new Container(viewSize(), new Vec(20));
+		UI.tag = "UI";
+		UI.position.z = 4;
+		
+		Label scoreLabel = new Label(Color.WHITE, new Font("Verdana", 0, 24), "Score: %s", score = new IntPointer(0));
+		scoreLabel.tag = "scoreLabel";
+		scoreLabel.setAnchor(Anchor.TL);
+		UI.topLeft.addChild(scoreLabel);
+		
+		addChildren(sky, asteroidList, bulletList, player, spawner, UI);
+	}
+	
+	public void update() {
+		runCollisions();
+	}
+	
+	private void runCollisions() {
+		List<Bullet> bullets = bulletList.getChildren(Bullet.class);
+		List<Asteroid> asteroids = asteroidList.getChildren(Asteroid.class);
+		
+		for(Asteroid a : asteroids) {
+			boolean split = false;
+			for(Bullet b : bullets) {
+				if(a.position.dst(b.position) < (a.small ? 20 : 40)) {
+					a.split();
+					b.freeSelf();
+					split = true;
+					score.add(a.small ? 20 : 40);
+					break;
+				}
+			}
+			
+			if(split) continue;
+			
+			if(a.position.dst(player.position) < (a.small ? 20 : 40)) {
+				asteroidList.removeAllChildren();
+				bulletList.removeAllChildren();
+				addChild(new DeathEffect());
+				//player.position.set(viewSize().half());
+				break;
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
